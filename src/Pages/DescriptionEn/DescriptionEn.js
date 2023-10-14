e62bbfb2117aa12c86f89d278c3b195072446947
Fxkhalil0 <fxkhalil0@gmail.com>
2023-10-15 01:26:04 +0300

    reveiw form ui

--- src/Pages/DescriptionEn/DescriptionEn.js
+++ src/Pages/DescriptionEn/DescriptionEn.js
@@ -6,12 +6,17 @@ import PeoIcon from '../../assets/Home/cards/people.png'
 import Image1 from '../../assets/Home/cards/card1.jpg'
 import Image2 from '../../assets/Home/cards/card2.jpg'
 import Image3 from '../../assets/Home/cards/card3.jpg'
+import User from '../../assets/default-user.png'
 import { Galleria } from 'primereact/galleria';
 import { Button } from 'primereact/button';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import FooterEn from "../Footer/FooterEn";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { FaStar } from 'react-icons/fa';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import { styled } from '@mui/system';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { DatePicker, Space } from 'antd';
@@ -25,6 +30,9 @@ function DescriptionEn() {
     const [activeIndex, setActiveIndex] = useState(0);
     const [startDate, setStartDate] = useState()
     const [endDate, setEndDate] = useState()
+    const [userRate, setUserRate] = useState(null)
+    const [rateHover, setRateHover] = useState(null)
+
     const dateFormat = 'DD/MM/YYYY';
 
     const responsiveOptions = [
@@ -128,6 +136,59 @@ function DescriptionEn() {
     const thumbnailTemplate = (item) => {
         return <img src={item.thumbnailImageSrc} alt={item.alt} />
     }
+    const blue = {
+        100: '#DAECFF',
+        200: '#b6daff',
+        400: '#3399FF',
+        500: '#007FFF',
+        600: '#0072E5',
+        900: '#003A75',
+    };
+
+    const grey = {
+        50: '#f6f8fa',
+        100: '#eaeef2',
+        200: '#d0d7de',
+        300: '#afb8c1',
+        400: '#8c959f',
+        500: '#6e7781',
+        600: '#57606a',
+        700: '#424a53',
+        800: '#32383f',
+        900: '#24292f',
+    };
+
+    const StyledTextarea = styled(TextareaAutosize)(
+        ({ theme }) => `
+        width: 70%;
+        height: 200px !important;
+        resize: none;
+        font-family: IBM Plex Sans, sans-serif;
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1.5;
+        padding: 12px;
+        border-radius: 12px 12px 0 12px;
+        color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+        background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+        border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+        box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+    
+        &:hover {
+          border-color: ${blue[400]};
+        }
+    
+        &:focus {
+          border-color: ${blue[400]};
+          box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+        }
+    
+        // firefox
+        &:focus-visible {
+          outline: 0;
+        }
+      `,
+    )
     return (<>
         <NavbarEn />
         <div className={style["main__div"]}>
@@ -309,30 +370,231 @@ function DescriptionEn() {
                             <DatePicker
                                 format={dateFormat}
                                 style={{ width: '47.5%', borderRadius: '4px', padding: '16.5px 14px' }}
-                                onChange={(date)=> {
+                                onChange={(date) => {
                                     console.log(typeof date.$d); // Date object
                                     setStartDate(date)
                                 }}
                                 value={startDate}
-                                />
-                                
+                            />
+
                             <DatePicker
                                 format={dateFormat}
-                                style={{ width: '47.5%', borderRadius: '4px', padding: '16.5px 14px' }} 
-                                onChange={(date)=> {
+                                style={{ width: '47.5%', borderRadius: '4px', padding: '16.5px 14px' }}
+                                onChange={(date) => {
                                     console.log(date); // Date object
                                     setEndDate(date)
                                 }}
                                 value={endDate}
-                                />
+                            />
                         </div>
                         <div className={style["booking__button"]}>
                             <button>Submit Booking</button>
                         </div>
                     </form>
-                    <h2>Booking Rules:</h2>
-                    <p>1)</p>
-                    <p></p>
+                    <div className={style["booking__rules"]}>
+                        <div className={style["rules__content"]}>
+                            <h2>Booking Rules:</h2>
+                            <p>1) After booking, the reservations office will contact you to process the payment</p>
+                            <p>2) If you need to cancel the tour you have booked, you can do so up to 24 hours before the scheduled tour date.</p>
+                            <p>3) To request a tour cancellation, please contact us at <span>Alayam@support.com</span>. We will guide you through the cancellation process and initiate a refund if the trip was pre-paid.</p>
+                            <p>4) There are no fees associated with the cancellation process.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={style["reveiw__section"]}>
+            <div className={style["reveiw__padding"]}>
+                <h2>Tell us your Experience</h2>
+                <div className={style["reveiw__container"]}>
+                    <form className={style["reveiw__form"]}>
+                        <div className={style["rate__box"]}>
+                            <div className={style["rate__stars"]}>
+                                <Typography className={style["label"]}>Room Rate</Typography>
+                                {[...Array(5)].map((star, i) => {
+                                    const ratingValue = i + 1
+                                    return <label>
+                                        <input className={style["input"]}
+                                            type='radio'
+                                            name='rating'
+                                            value={ratingValue}
+                                            onClick={() => {
+                                                setUserRate(ratingValue)
+                                            }}
+
+                                        ></input>
+                                        <FaStar className={style["star"]}
+                                            size={25}
+                                            color={ratingValue <= (userRate || rateHover) ? "#ffc107" : "#e4e5e9"}
+                                            onMouseEnter={() => {
+                                                setRateHover(ratingValue)
+                                            }}
+                                            onMouseLeave={() => {
+                                                setRateHover(null)
+                                            }}
+                                        />
+                                    </label>
+                                })}
+                            </div>
+                            <div className={style["rate__stars"]}>
+                                <Typography className={style["label"]}>Room Rate</Typography>
+                                {[...Array(5)].map((star, i) => {
+                                    const ratingValue = i + 1
+                                    return <label>
+                                        <input className={style["input"]}
+                                            type='radio'
+                                            name='rating'
+                                            value={ratingValue}
+                                            onClick={() => {
+                                                setUserRate(ratingValue)
+                                            }}
+
+                                        ></input>
+                                        <FaStar className={style["star"]}
+                                            size={25}
+                                            color={ratingValue <= (userRate || rateHover) ? "#ffc107" : "#e4e5e9"}
+                                            onMouseEnter={() => {
+                                                setRateHover(ratingValue)
+                                            }}
+                                            onMouseLeave={() => {
+                                                setRateHover(null)
+                                            }}
+                                        />
+                                    </label>
+                                })}
+                            </div>
+                            <div className={style["rate__stars"]}>
+                                <Typography className={style["label"]}>Room Rate</Typography>
+                                {[...Array(5)].map((star, i) => {
+                                    const ratingValue = i + 1
+                                    return <label>
+                                        <input className={style["input"]}
+                                            type='radio'
+                                            name='rating'
+                                            value={ratingValue}
+                                            onClick={() => {
+                                                setUserRate(ratingValue)
+                                            }}
+
+                                        ></input>
+                                        <FaStar className={style["star"]}
+                                            size={25}
+                                            color={ratingValue <= (userRate || rateHover) ? "#ffc107" : "#e4e5e9"}
+                                            onMouseEnter={() => {
+                                                setRateHover(ratingValue)
+                                            }}
+                                            onMouseLeave={() => {
+                                                setRateHover(null)
+                                            }}
+                                        />
+                                    </label>
+                                })}
+                            </div>
+                            <div className={style["rate__stars"]}>
+                                <Typography className={style["label"]}>Room Rate</Typography>
+                                {[...Array(5)].map((star, i) => {
+                                    const ratingValue = i + 1
+                                    return <label>
+                                        <input className={style["input"]}
+                                            type='radio'
+                                            name='rating'
+                                            value={ratingValue}
+                                            onClick={() => {
+                                                setUserRate(ratingValue)
+                                            }}
+
+                                        ></input>
+                                        <FaStar className={style["star"]}
+                                            size={25}
+                                            color={ratingValue <= (userRate || rateHover) ? "#ffc107" : "#e4e5e9"}
+                                            onMouseEnter={() => {
+                                                setRateHover(ratingValue)
+                                            }}
+                                            onMouseLeave={() => {
+                                                setRateHover(null)
+                                            }}
+                                        />
+                                    </label>
+                                })}
+                            </div>
+                        </div>
+                        <Box
+                            component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        // style={{ marginTop: '20px' }}
+                        >
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Name"
+                                placeholder="Your name"
+                                style={{ width: '100%', marginBottom: '15px' }}
+
+                            />
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Email"
+                                placeholder="Your email address"
+                                style={{ width: '100%' , marginBottom: '30px'}}
+
+                            />
+                        </Box>
+                        <StyledTextarea
+                            aria-label="minimum height"
+                            minRows={3}
+                            placeholder="Add your commenr"
+                        />
+                    </form>
+                    <div className={style['reveiw__card']}>
+                        <div className={style['padding__div']}>
+                            <div className={style['reveiw__content']}>
+                                <div className={style['stars']}>
+                                    <div className={style["card__rate__stars"]}>
+                                        <Typography className={style["label"]}>Room Rate</Typography>
+                                        {[...Array(5)].map((star, i) => {
+                                            const ratingValue = i + 1
+                                            return <label>
+                                                <input className={style["input"]}
+                                                    type='radio'
+                                                    name='rating'
+                                                    value={ratingValue}
+                                                    onClick={() => {
+                                                        setUserRate(ratingValue)
+                                                    }}
+
+                                                ></input>
+                                                <FaStar className={style["star"]}
+                                                    size={25}
+                                                    color={ratingValue <= (userRate || rateHover) ? "#ffc107" : "#e4e5e9"}
+                                                    onMouseEnter={() => {
+                                                        setRateHover(ratingValue)
+                                                    }}
+                                                    onMouseLeave={() => {
+                                                        setRateHover(null)
+                                                    }}
+                                                />
+                                            </label>
+                                        })}
+                                    </div>
+                                </div>
+                                <div className={style['cleint__reveiw']}>
+                                    <div className={style['cleint__info']}>
+                                        <img src={User} alt='' />
+                                        <div className={style['name__date']}>
+                                            <h3>Mohammed Elsaid</h3>
+                                            <p>Jan 20, 2023</p>
+                                        </div>
+                                    </div>
+                                    <p>I quickly found the right tour for me, but I had a few questions about the hotel, I wrote to tech support and they answered all my questions within an hour. The vacation itself was perfect. Thank you very much. I will come back again and again.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
